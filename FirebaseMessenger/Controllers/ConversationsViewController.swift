009e0bfbf31c35f2579a0d25bb2801ee240253a0
Tim Kalaiko <a.kalayko@gmail.com>
2022-12-28 10:10:42 +0200

    initial setup

---
+++ FirebaseMessenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseMessenger
+//
+//  Created by Tim on 28.12.2022.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
